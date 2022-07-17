b9cbcef70207cc192ec4ea10fcbcad5aa515df84
Justin Bui <40068612+fantasticfishman@users.noreply.github.com>
2022-07-17 02:31:14 -0700

    added scroll stuff
    need to fix stars and titlecards

--- src/App.js
+++ src/App.js
@@ -2,11 +2,11 @@ import React, { Component } from "react";
 import Navbar from "./Components/Navbar";
 import Section from "./Components/Section";
 import ImageGallery from "react-image-gallery";
-import { ParallaxProvider } from 'react-scroll-parallax';
-import { Parallax } from 'react-scroll-parallax';
+import { ParallaxProvider } from "react-scroll-parallax";
+import { Parallax } from "react-scroll-parallax";
 import TitleCard from "./Components/Titlecard";
-import slide from "./carouselpics/slide.png";
-import '../node_modules/react-image-gallery/styles/css/image-gallery.css';
+import star from "./picsandstuff/star.png";
+import "../node_modules/react-image-gallery/styles/css/image-gallery.css";
 import "./App.css";
 
 const images = [
@@ -27,44 +27,75 @@ class App extends Component {
   render() {
     return (
       <ParallaxProvider>
-      <div className="App">
-        <Navbar />
-        <Section
-          title="Section 1"
-          subtitle="cringe"
-          dark={true}
-          id="section1"
-          carousel={<ImageGallery items={images} />}
-        />
-        <Parallax>
-        <TitleCard />
-        </Parallax>
-        {/* <ImageGallery items={images} /> */}
-        <Section
-          title="Section 2"
-          subtitle="cringe"
-          dark={true}
-          id="section2"
-        />
-        <Section
-          title="Section 3"
-          subtitle="cringe"
-          dark={true}
-          id="section3"
-        />
-        <Section
-          title="Section 4"
-          subtitle="cringe"
-          dark={true}
-          id="section4"
-        />
-        <Section
-          title="Section 5"
-          subtitle="cringe"
-          dark={true}
-          id="section5"
-        />
-      </div>
+        <div className="App">
+          <Navbar />
+          <Section
+            title="rem is the best waifu"
+            subtitle="rem is the best waifu"
+            dark={true}
+            id="section1"
+            carousel={<ImageGallery items={images} />}
+          />
+          <div className="stars">
+            <Parallax>
+              <img id="star1" alt="a funny star" src={star} />
+            </Parallax>
+            {/* <Parallax rotate={[-500,500]} translateY={-800} >
+              <img id = "star2" alt="a funny star" src={star}/>
+          </Parallax>
+          <Parallax rotate={[-500,500]} translateY={-800} >
+              <img id = "star3" alt="a funny star" src={star}/>
+          </Parallax>
+          <Parallax rotate={[-500,500]} translateY={-800} >
+              <img id = "star4" alt="a funny star" src={star}/>
+          </Parallax> */}
+          </div>
+          <Parallax translateX={[-20,20]}>
+            <div className="titlecard">
+              <TitleCard title="AMONG US!!!!" />
+            </div>
+          </Parallax>
+          {/* <ImageGallery items={images} /> */}
+          <Section
+            title="Section 2"
+            subtitle="cringe"
+            dark={true}
+            id="section2"
+          />
+          <Parallax translateX={[-20, 20]}>
+            <div className="titlecard">
+              <TitleCard title="AMONG US!!!!" />
+            </div>
+          </Parallax>
+          <Section
+            title="Section 3"
+            subtitle="cringe"
+            dark={true}
+            id="section3"
+          />
+          <Parallax translateX={[-40, 40]}>
+            <div className="titlecard">
+              <TitleCard title="AMONG US!!!!" />
+            </div>
+          </Parallax>
+          <Section
+            title="Section 4"
+            subtitle="cringe"
+            dark={true}
+            id="section4"
+          />
+          <Parallax translateX={[-20, 20]}>
+            <div className="titlecard">
+              <TitleCard title="AMONG US!!!!" />
+            </div>
+          </Parallax>
+          <Section
+            title="Section 5"
+            subtitle="cringe"
+            dark={true}
+            id="section5"
+          />
+        </div>
       </ParallaxProvider>
     );
   }
--- src/Components/Titlecard.js
+++ src/Components/Titlecard.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function TitleCard({title}){
 return(
     <div className="content">
-        {title}
+        <h1>{title}</h1>
     </div>
 );
 }
