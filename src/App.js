1f15fe044cb7fb6190ab932e663d7bbe7419a380
Justin Bui <40068612+fantasticfishman@users.noreply.github.com>
2022-08-11 05:27:53 -0700

    delete app1

--- src/App.js
+++ src/App.js
@@ -1,185 +1,152 @@
 import React, { Component } from "react";
-import Navbar from "./Components/Navbar";
-import Section from "./Components/Section";
-import ImageGallery from "react-image-gallery";
 import { ParallaxProvider } from "react-scroll-parallax";
 import { Parallax } from "react-scroll-parallax";
-import slide from "./carouselpics/slide.png";
-import TitleCard from "./Components/Titlecard";
-import star from "./picsandstuff/star.png";
-import "../node_modules/react-image-gallery/styles/css/image-gallery.css";
+import Rem from "./images/test.JPG";
+import { Wave } from "react-animated-text";
+import { Timeline, Events, UrlButton, ImageEvent } from "@merc/react-timeline";
+import staff from "./images/staff.jpg";
+import { Github } from "react-bootstrap-icons";
+import { Envelope } from "react-bootstrap-icons";
+import { Linkedin } from "react-bootstrap-icons";
 import "./App.css";
-
-const aboutimages = [
-  {
-    original: "https://picsum.photos/id/1018/1000/600/",
-    thumbnail: "https://picsum.photos/id/1018/250/150/",
-  },
-  {
-    original: "https://picsum.photos/id/1015/1000/600/",
-    thumbnail: "https://picsum.photos/id/1015/250/150/",
-  },
-  {
-    original: "https://picsum.photos/id/1019/1000/600/",
-    thumbnail: "https://picsum.photos/id/1019/250/150/",
-  },
-];
 
 class App extends Component {
   render() {
     return (
       <ParallaxProvider>
         <div className="App">
-          <Parallax opacity={[2, 0]}>
-            <img id="header" src={slide}></img>
-          </Parallax>
-          <Navbar />
-          <Parallax translateX={[-20, 0]} scale={[, 1.5]} opacity={[0.15, 1.5]}>
-            <div className="titlecard">
-              <TitleCard title="About Me" />
-            </div>
-          </Parallax>
-          <Parallax opacity={[0.55, 2]}>
-            <Section
-              title="I'm a second year student at UCSD studying Computer Science."
-              list={
-                <ul>
-                  <li>
-                    In my free time, I enjoy playing the guitar, hanging out
-                    with friends, or coding.
-                  </li>
-                  <li>
-                    I hope to start a career in Web Development in future.
-                  </li>
-                  <li>among us!!</li>
-                  <li>among us!!!</li>
-                </ul>
-              }
-              dark={true}
-              id="section1"
-              carousel={<ImageGallery items={aboutimages} />}
-            />
-          </Parallax>
-          <div className="stars">
-            <Parallax translateY={[-20, 20]}>
-              {/* <img id="star1" alt="a funny star" src={star} /> */}
+          <div className="titlepage">
+            <Parallax opacity={[2, 0]}>
+              <h1>
+                <Wave
+                  text="Justin Bui"
+                  effect="pop"
+                  effectChange=".9"
+                  speed="10"
+                />
+              </h1>
+              <h2>Developer. Designer. Leader.</h2>
+              <h3>Coming soon (hopefully) to a workplace near you.</h3>
             </Parallax>
           </div>
-          <Parallax translateX={[-20, 0]} scale={[, 1.5]} opacity={[0.15, 1.5]}>
-            <div className="titlecard">
-              <TitleCard title="Development" />
+          <div className="about">
+            <Parallax opacity={[0.2, 2.5]}>
+              <img src={Rem} alt="the best waifu"></img>
+              <h1>
+                Hi. I'm Justin, a student studying Computer Science at
+                University of California, San Diego. I have experience with
+                Java, JavaScript, Python, HTML, CSS, and React, along with
+                leadership and organizational experience.
+              </h1>
+            </Parallax>
+          </div>
+          <Parallax opacity={[0, 3]}>
+            <hr></hr>
+          </Parallax>
+          <Parallax opacity={[0, 3]}>
+            <div className="timeline">
+              <Timeline>
+                <Events>
+                  <ImageEvent
+                    date="2018-2020-WebDev"
+                    text="I was in charge of building and maintaining my organization's website, gaining experience with HTML, CSS, Github Pages, as well as working with a team"
+                  >
+                    <div>
+                      <UrlButton href="https://ca-935th.com/" target="_blank">
+                        It's still up and going!
+                      </UrlButton>
+                    </div>
+                  </ImageEvent>
+                  <ImageEvent
+                    date="2021-Leadership"
+                    text="I was selected to lead a team to document/advertise group activities for a club of 160+ students.
+                    I organized the capturing of events through photo and video footage, and publicizing/advertising our club to outside organizations.
+                    worked on, led, organized the creation of monthly newsletters/videos, as well as a website/social media.
+                    "
+                  >
+                    <div>
+                      <UrlButton
+                        href="https://drive.google.com/file/d/1Sl6SpVuzupJKdfWgxJwRj2Tz7eK9SlHw/view?usp=sharing"
+                        target="_blank"
+                      >
+                        I even led the creation of a yearbook!
+                      </UrlButton>
+                    </div>
+                  </ImageEvent>
+                  <ImageEvent
+                    date="2021-Leadership"
+                    text="I was 1 out of 9 students selected to lead that same organization, 
+                    organizing many events including training camps, an awards ceremony of 100+ attendees, 
+                    social events, contests/competitions, workshops, and more. I learned to work as a team, lead others, and shoulder responsibility.
+                    That's me on the right by the way."
+                    src={staff}
+                  />
+                  <ImageEvent
+                    date="2021-Leadership"
+                    text='For my efforts that year, 
+                    I recieved the Veterans of Foreign Wars Citation and Medal Award, 
+                    "in special recognition of outstanding achievements and exceptional leadership ability.'
+                  />
+                  <ImageEvent
+                    date="Fall 2021-Projects"
+                    text="With a team, I created an exercise tracker app using the MERN stack, gaining frontend experience with React, as well as backend experience with MongoDB.
+                    I also gained experience with working with deadlines and collaborating with a development team. Unfortunately the database is down so I don't have a working demo,
+                    but the code is here."
+                  >
+                    <div>
+                      <UrlButton
+                        href="https://github.com/thomas-gg/mern-template"
+                        target="_blank"
+                      >
+                        MERN Stack Workout Tracker
+                      </UrlButton>
+                    </div>
+                  </ImageEvent>
+                  <ImageEvent
+                    date="2022-Projects"
+                    text="I created a wallpaper scraper using Python, it takes user input and automatically batch downloads wallpapers for them based on their search term."
+                  >
+                    <div>
+                      <UrlButton
+                        href="https://github.com/fantasticfishman/scraper"
+                        target="_blank"
+                      >
+                        Python Wallpaper Scraper
+                      </UrlButton>
+                    </div>
+                  </ImageEvent>
+                  <ImageEvent
+                    date="2022-Projects"
+                    text='I created a Discord Bot using the Discord.js API, gaining experience with advanced Javascript, such as callbacks.'
+                    >
+                    <div>
+                      <UrlButton
+                        href="https://github.com/fantasticfishman/rembot"
+                        target="_blank"
+                      >
+                        Discord Bot
+                      </UrlButton>
+                    </div>
+                  </ImageEvent>
+                  <ImageEvent
+                    date="2022-Research"
+                    text="This year, I'll be conducting guided research with a UCSD professor."
+                  />
+                </Events>
+              </Timeline>
             </div>
           </Parallax>
-          <Parallax opacity={[0.55, 2]}>
-            <Section
-              title="Some examples of what I've done, and the tools required."
-              list={
-                <ul>
-                  <li>A unit website for my school's AFJROTC program</li>
-                  <ul>
-                    <li>Github Pages</li>
-                    <li>Regular HTML/CSS</li>
-                  </ul>
-                  <li>A Discord Bot to annoy my friends</li>
-                  <ul>
-                    <li>Discord.js</li>
-                  </ul>
-                  <li>A Workout Tracker App</li>
-                  <ul>
-                    <li>React/Node.JS for frontend</li>
-                    <li>MongoDB/Express for backend</li>
-                  </ul>
-                </ul>
-              }
-              dark={true}
-              id="section2"
-              carousel={<ImageGallery items={aboutimages} />}
-            />
-          </Parallax>
-          <Parallax translateX={[-20, 0]} scale={[, 1.5]} opacity={[0.15, 1.5]}>
-            <div className="titlecard">
-              <TitleCard title="Leadership" />
+          <hr></hr>
+          <div className="contact">
+            <h1>
+                Need something done?
+            </h1>
+            <div className="icons">
+                <Envelope size={96}/>
+                <Github size={96}/>
+                <Linkedin size={96}/>
             </div>
-          </Parallax>
-          <Parallax opacity={[0.55, 2]}>
-            <Section
-              title='"People who enjoy meetings should not be in charge of anything."- Thomas Sowell'
-              list={
-                <ul>
-                  <li>AFJROTC Senior Staff</li>
-                  <ul>
-                    <li>
-                      1/9 students selected to lead high school Air Force Junior
-                      Reserve Officer Training Corps program of 160+ students
-                    </li>
-                    <li>
-                      organized many events, including training camps, awards
-                      ceremony, social events,
-                      contests/competitions, workshops, and more
-                    </li>
-                  </ul>
-                  <li>AFJROTC Public Affairs Squadron Commander</li>
-                  <ul>
-                    <li>
-                      Selected to lead a team of 15 students to
-                      document unit activities
-                    </li>
-                    <li>
-                      Organized the capturing of
-                      events through photo/video footage
-                    </li>
-                    <li>
-                      Organized creation of monthly
-                      newsletters/videos, a yearbook, and unit website/social
-                      media
-                    </li>
-                    <li>
-                      In charge of publicizing/advertising unit to outside
-                      organizations
-                    </li>
-                  </ul>
-                </ul>
-              }
-              dark={true}
-              id="section3"
-              carousel={<ImageGallery items={aboutimages} />}
-            />
-          </Parallax>
-          <Parallax
-            translateX={[-20, 40]}
-            scale={[, 1.5]}
-            opacity={[0.15, 1.5]}
-          >
-            <div className="titlecard">
-              <TitleCard title="Research" />
-            </div>
-          </Parallax>
-          <Parallax opacity={[0.55, 2]}>
-            <Section
-              title='"If we knew what we were doing, it would not be called research, would it? -Albert Einstien"'
-              subtitle="jk it wont start until fall lmao"
-              dark={true}
-              id="section4"
-              carousel={<ImageGallery items={aboutimages} />}
-            />
-          </Parallax>
-          <Parallax
-            translateX={[-20, 40]}
-            scale={[, 1.5]}
-            opacity={[0.15, 1.5]}
-          >
-            <div className="titlecard">
-              <TitleCard title="Contact Me" />
-            </div>
-          </Parallax>
-          <Parallax opacity={[0.55, 2]}>
-            <Section
-              title="Need something done?"
-              subtitle="PLEASE ILL DO ANYTHING"
-              dark={true}
-              id="section5"
-            />
-          </Parallax>
+          </div>
         </div>
       </ParallaxProvider>
     );
