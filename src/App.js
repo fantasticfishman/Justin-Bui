be1aba27255f03536fb9a426358a6a4638233d9f
Justin Bui <40068612+fantasticfishman@users.noreply.github.com>
2023-12-27 01:45:44 -0800

    reformatted, added images

--- src/App.js
+++ src/App.js
@@ -2,7 +2,9 @@ import React, { Component } from "react";
 import { ParallaxProvider } from "react-scroll-parallax";
 import { Parallax } from "react-scroll-parallax";
 import Me from "./images/me.JPG";
-import { Wave, Random, } from "react-animated-text";
+import litfit from "./images/litfit.png";
+import pantrypal from "./images/pantrypal.png";
+import { Wave, Random } from "react-animated-text";
 import {
   Timeline,
   Events,
@@ -46,7 +48,7 @@ class App extends Component {
                 <Random
                   text="Scroll for more!"
                   effect="jump"
-                  effectDirection = "up"
+                  effectDirection="up"
                   effectChange=".05"
                 />
               </h4>
@@ -69,78 +71,66 @@ class App extends Component {
               <Timeline theme={themes.roli}>
                 <Events>
                   <ImageEvent
-                    date="2018-2020-Projects/Leadership"
-                    text="I was in charge of building and maintaining my organization's website, gaining experience with HTML, CSS, Github Pages, as well as working with a team."
-                  >
-                    <div>
-                      <UrlButton href="https://ca-935th.com/" target="_blank">
-                        It's still up and going!
-                      </UrlButton>
-                    </div>
-                  </ImageEvent>
-                  <ImageEvent
                     date="2021-Leadership"
-                    text="I was selected to lead a team to document/advertise group activities for a club of 160+ students.
-                    I organized the capturing of events through photo and video footage, and publicizing/advertising our club to outside organizations. I also
-                    organized the creation of monthly newsletters/videos, as well as a website and social media accounts.
-                    "
+                    text="I was 1 out of 9 students
+                    selected to lead my high school's AFJROTC organization,
+                    organizing many events including training camps, an awards
+                    ceremony of 100+ attendees, social events,
+                    contests/competitions, workshops, and more. For my specific
+                    role, I managed capturing footage of events, our social media accounts, creation of monthly newsletters, and
+                    advertising/publicizing our club to outside organizations."
+                    src={staff}
                   >
                     <div>
                       <UrlButton
                         href="https://drive.google.com/file/d/1Sl6SpVuzupJKdfWgxJwRj2Tz7eK9SlHw/view?usp=sharing"
                         target="_blank"
                       >
-                        I even led the creation of a yearbook!
+                        I also led the creation of a yearbook!
                       </UrlButton>
                     </div>
                   </ImageEvent>
                   <ImageEvent
-                    date="2021-Leadership"
-                    text="I was 1 out of 9 students selected to lead that same organization, 
-                    organizing many events including training camps, an awards ceremony of 100+ attendees, 
-                    social events, contests/competitions, workshops, and more. I learned to work as a team, lead others, and shoulder responsibility.
-                    That's me on the right by the way."
-                    src={staff}
-                  />
-                  <ImageEvent
-                    date="2021-Leadership"
-                    text='For my efforts that year, 
+                    date="2018-2020-Projects/Leadership"
+                    text='I was also in charge of our website, gaining experience with HTML, CSS, Github Pages, as well as working with a team. For my efforts with all of the above, 
                     I recieved the AFJROTC Captain Kenneth R. Cherry Scholarship Award
                     of $1000 awarded for a "desire to serve", "integrity and excellence", and "strong leadership skills", as well as the Veterans of Foreign Wars Citation and Medal Award, 
                     "in special recognition of outstanding achievements and exceptional leadership ability".'
-                  />
-                  <ImageEvent
-                    date="2021-Projects"
-                    text="With a team, I created an exercise tracker app using the MERN stack, gaining experience with React, Express, MongoDB, and HTTP requests that connect those. 
-                    We also implemented a login system that allows users to create accounts, and logging in authenticates them with a JSON web token so they can edit their info securely.
-                    I also gained experience with working with deadlines and collaborating with a development team."
                   >
                     <div>
-                      <UrlButton
-                        href="https://github.com/thomas-gg/mern-template"
-                        target="_blank"
-                      >
-                        MERN Stack Workout Tracker
+                      <UrlButton href="https://ca-935th.com/" target="_blank">
+                        Our website is still up and going!
                       </UrlButton>
                     </div>
                   </ImageEvent>
                   <ImageEvent
+                    date="2021-Projects"
+                    text="With a team, I created an exercise tracker app using the MERN stack, gaining experience with React, Express, MongoDB, and HTTP. 
+                    We also implemented a login system that allows users to create accounts with details hashed using BCrypt, and logging in securely authenticates using JSONWebToken."
+                    src={litfit}
+                  ></ImageEvent>
+                  <ImageEvent
                     date="2022-Leadership"
                     text="I planned and conducted socials, presentations, and leadership workshops
-                    for the UCSD Computer Science and Engineering Department's student engagement program aimed at new CS first years and transfers, as a SWELL Peer Leader."
+                    for the UCSD Computer Science and Engineering Department's student engagement program aimed at new CS first years and transfers."
                   />
-                  <ImageEvent
-                    date="2022-Research"
+                  <ImageEvent>
                     text="I conducted a research project with UCSD professors as part of the Early Research Scholars Program. 
                     We explored how advances in realistic rendering technology apply to the problem of inverse rendering, taking images and turning them into 3d scenes."
-                  />
+                    <div>
+                      <UrlButton href="https://drive.google.com/file/d/1GGm0_OAGLQtPTFxK_84pzkXXieC4QOt6/view?usp=sharing" target="_blank">
+                        Find our poster here:
+                      </UrlButton>
+                    </div>
+                    </ImageEvent>
                   <ImageEvent
                     date="2023-Experience"
-                    text="I worked as an Release Engineer intern for Cubic Transportation Systems, gaining industry experience with DevOps tools like Docker, Jenkins, and Azure Devops, as well as Python scripting and server hosting."
+                    text="I worked over summer as an Release Engineer intern for Cubic Transportation Systems, gaining industry experience with DevOps tools like Docker, Jenkins, and Azure Devops, as well as Python scripting and server hosting."
                   />
                   <ImageEvent
                     date="2023-Projects"
-                    text="I was the project leader for a team of 5, where we created a multi-tiered Java application leveraging the ChatGPT/Whisper/DALLE APIs to generate recipes and images for an end user based on spoken user input, with user data being stored in MongoDB."
+                    text="I was the project leader for a team of 5, where we created a multi-tiered Java application leveraging the ChatGPT/Whisper/DALLE APIs to generate recipes and images for an end user based on spoken user input, with user data being stored in MongoDB. I organized and delegated all tasks, and planned/led meetings, as well as shouldered my own share of the development work."
+                    src={pantrypal}
                   />
                 </Events>
               </Timeline>
